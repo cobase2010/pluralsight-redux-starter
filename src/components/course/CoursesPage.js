f8836c29b2f360930efaf91f737be019c7c7ef53
cobase2010 <cobase2010@live.com>
2016-11-14 13:25:40 -0800

    Initial commit before Redux code.

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Tis application uses React, Redux and React Router in ES6 and a variety of other helpful libraries.</p>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
