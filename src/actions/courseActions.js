bb674c6c5aefeefbb4b5b9b05c961aaf195a24f4
cobase2010 <cobase2010@live.com>
2016-11-15 19:57:32 -0800

    Check in after creating course form.

---
+++ src/actions/courseActions.js
@@ -0,0 +1,17 @@
+import * as types from './actionTypes';
+import CourseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(courses) {
+  // debugger;
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return CourseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render () {
+    // debugger;
+    const {courses} = this.props;
     return (
       <div>
-        <h1>About</h1>
-        <p>Tis application uses React, Redux and React Router in ES6 and a variety of other helpful libraries.</p>
+        <h1>Courses</h1>
+        <CourseList courses={courses}/>
       </div>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  // debugger;
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
